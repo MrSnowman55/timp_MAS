7c9c99d80f1bf559b8470f599dae271107d879a1
MrSnowman55 <marjasov-work@mail.ru>
2021-06-10 05:30:42 +0700

    update pr1

---
+++ pr1/03_pow.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    double x, y;
+    (void)scanf("%lf", &x);
+    (void)scanf("%lf", &y);
+    printf ("%lf", pow(x, y));
+    return 0;
+}   
